ea9d892479a705b392761a4f15556920550bf78f
francomac <franco.mcarballo@outlook.com>
2022-02-21 11:04:31 -0600

    vuex initial setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import App from "./App";
+import { store } from './store/'
 
 // new Vue({
 //     render: h => h(App)
 // }).$mount('#app')
 
 const app = createApp(App);
-
+app.use(store)
 app.mount("#app");
