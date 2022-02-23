97c082a6285ae245499fc93c3299d461fad570f9
francomac <franco.mcarballo@outlook.com>
2022-02-22 22:37:56 -0600

    create menu buttons |  clean token when loggedout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,10 +3,12 @@
     <a href="/" class="active item"> Image Storage </a>
 
     <div class="right menu">
-      <div v-if="isLoggedIn">
-
+      <div v-if="!isLoggedIn" class="horizontal">
+        <a class="item">Galleries</a>
+        <a class="item">Upload</a>
+        <a class="item" @click="logout">Logout</a>
       </div>
-      
+
       <a v-else href="#" class="ui item" @click="login">Login</a>
     </div>
   </div>
@@ -18,8 +20,13 @@ import { mapActions, mapGetters } from "vuex";
 export default {
   name: "AppHeader",
   methods: mapActions("auth", ["login"]),
-  computed: mapGetters("auth", ["isLoggedIn"]),
+  computed: mapGetters("auth", ["isLoggedIn", "logout"]),
 };
 </script>
 
-<style></style>
+<style scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
+</style>
