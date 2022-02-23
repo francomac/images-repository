dbfe9291739f117a353f6552c3aa16041daa8787
francomac <franco.mcarballo@outlook.com>
2022-02-23 11:18:25 -0600

    fetch images from imgur api

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from "qs";
+import axios from "axios";
 
 const CLIENT_ID = "4cbf12c50780b3f";
 const ROOT_URL = "https://api.imgur.com";
@@ -6,11 +7,21 @@ const ROOT_URL = "https://api.imgur.com";
 const login = () => {
   const queryString = {
     client_id: CLIENT_ID,
-    response_type: "token"
+    response_type: "token",
   };
 
   window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(queryString)}`;
 };
+
+const fetchImages = (token) => {
+  return axios.get(`${ROOT_URL}/3/account/me/images`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
 export default {
   login,
+  fetchImages,
 };
