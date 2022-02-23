8166d9307f7b5fb98def2463466c9bffeb12e8a9
francomac <franco.mcarballo@outlook.com>
2022-02-23 11:19:30 -0600

    render images onto images list component

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>Image List</div>
+  <div>
+    <img v-for="image in allImages" :key="image" :src="image.link" alt="" />
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
 export default {
   name: "ImageList",
+
+  computed: mapGetters("images", ["allImages"]),
+  methods: mapActions("images", ["fetchImages"]),
+  created() {
+    this.fetchImages();
+  },
 };
 </script>
 
