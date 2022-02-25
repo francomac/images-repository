79596f0115ebd130393c18cdf1aae8a310aa201a
francomac <franco.mcarballo@outlook.com>
2022-02-24 20:28:47 -0600

    new upload images form

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>Upload form</div>
+  <div class="dropper">
+    <input
+      type="file"
+      @change="uploadImages($event.target.files)"
+      multiple
+      accept="image/*"
+    />
+    <span> Drag files here! </span>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
-  name: "UploadFormm",
+  name: "UploadForm",
+  methods: mapActions("images", ["uploadImages"]),
 };
 </script>
 
-<style></style>
+<style scoped>
+.dropper {
+  height: 30vh;
+  border: 2px dashed black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+.dropper:hover {
+  background-color: #eee;
+}
+input {
+  width: 100%;
+  height: 30vh;
+  position: absolute;
+  opacity: 0;
+}
+</style>
